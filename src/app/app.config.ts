fed2accb14f0f80c83acc020aeb4ebbcea4e37c0
Ramsey Louder <RamseyLouder@gmail.com>
2024-08-07 09:35:14 -0400

    Google Oauth

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,38 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {
+  SocialLoginModule,
+  SocialAuthServiceConfig,
+  GoogleLoginProvider,
+} from "@abacritt/angularx-social-login";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    //We are building a provider. Version 2.2.0 of package doesn't provide one.
+    {
+    provide: 'SocialAuthServiceConfig',
+    useValue: {
+      autoLogin: false,
+      providers: [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider(
+            //Remove the .apps.googleusercontent.com from the client id
+            //MAKE SURE TO HIDE IT FROM GITHUB
+            "Replace with client id"
+          ),
+        },
+      ],
+      onError: (err) => {
+        debugger;
+        console.error(err);
+      },
+    } as SocialAuthServiceConfig,
+  },
+  //more providers can go here
+]
 };
+
---
+++ src/app/student-component/student-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './student-component.component.html',
+  styleUrl: './student-component.component.css'
+})
+export class StudentComponentComponent {
+
+}
